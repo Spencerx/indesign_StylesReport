ada189b55bb86b8ab602edfc4f975b0d7f01cfe3
nelliemckesson <nellie.mckesson@macmillan.com>
2014-10-09 11:38:23 -0400

    adding v1 files

---
+++ README.md
@@ -0,0 +1,10 @@
+StylesReport for InDesign
+==========================
+
+NOTE: Has not yet been extensively tested.
+
+Returns a sequential list of paragraph styles in use, for each paragraph, as well as either the first 20 characters of the paragraph text or the full paragraph if it is less than 20 chars.
+
+To be used for comparing styles used in the InDesign file vs. styles used in the Word file.
+
+Mac Users: Install in Macintosh HD/Applications/Adobe InDesign FOO/Scripts/Scripts Panel/
---
+++ StylesReport.jsx
@@ -0,0 +1,47 @@
+// Returns a sequential list of paragraph styles in use, for each paragraph
+// as well as either the first 20 characters of the paragraph text 
+// or the full paragraph if it is less than 20 chars
+
+(function() {
+if (app.documents.length > 0) {
+processDocument(app.documents[0]);
+}
+
+function processDocument(aDoc) {
+var curr, last = [], len = 0;
+var page = aDoc.pages[0];
+var myDoc = app.activeDocument;
+var myStory = myDoc.stories.item(0);
+var myParas = myStory.paragraphs;
+var arr = [];
+for (var k = 0; myParas.length > k; k++) {
+  var currStyle = myParas[k].appliedParagraphStyle.name;
+  var charCount = myParas[k].characters.length;
+  if (charCount < 20)
+  {
+    var currText = myParas[k].contents;
+  } else {
+    var myStartCharacter = myParas[k].characters.item(0);
+    var myEndCharacter = myParas[k].characters.item(20);
+    var currText = myParas[k].texts.itemByRange(myStartCharacter, myEndCharacter).contents;
+  }
+  var value = currStyle + " : " + currText;
+  arr.push(value);
+}
+
+var myTextFile = new File(aDoc.filePath+"/StylesReport.txt");
+myTextFile.open("w");
+if (!myTextFile.open("w"))  
+    {  
+       alert ("Uh-oh something went wrong :(");  
+       exit(0);  
+    }
+for (var k = 0; arr.length > k; k++) {
+  myTextFile.seek(0, 2);
+  myTextFile.write(arr[k] + "\n");
+}
+myTextFile.close(); 
+
+}
+}())
+
